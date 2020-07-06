6043f3e31f9c6652faa944a81b0b645218192ea7
Maxwell DeMaio <maxdman321@gmail.com>
2020-07-06 15:24:55 -0400

    Implemented Django forms, prepopulated them with title/contents

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.urls import reverse
 from . import util
 
+class NewEditForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=100)
+    contents = forms.CharField(label="Contents", widget=forms.Textarea)
 
 def index(request):
     """ Homepage """
@@ -27,15 +30,40 @@ def view_entry(request, entry):
 def edit_entry(request, entry):
     """ Allow a user to edit an entry """
     if request.method == "POST":
-        return redirect(reverse('viewEntry', kwargs={'entry': entry}))
+        # TODO Make changes for the edits made
+
+        # Create a form instance and populate it with data from the request
+        form = NewEditForm(request.POST)
+
+        # Check whether form is valid
+        if form.is_valid():
+            # TODO Process the data and redirect
+            title = form.cleaned_data['title']
+            contents = form.cleaned_data['contents']
+            return redirect(reverse('viewEntry', kwargs={'entry': entry}))
+        else:
+            title = util.get_title(entry)
+            contents = util.read_contents(entry)
+            form = NewEditForm({"title": title, "contents": contents})
+            return render(request, "encyclopedia/edit.html", {
+                "form": form,
+                "entry": entry,
+                "contents": contents,
+                "title": title
+            })
     else:
         if util.get_entry(entry):
+            title = util.get_title(entry)
+            contents = util.read_contents(entry)
+            form = NewEditForm({"title": title, "contents": contents})
             return render(request, "encyclopedia/edit.html", {
+                "form": form,
                 "entry": entry,
-                "contents": util.read_contents(entry),
-                "title": util.get_title(entry)
+                "contents": contents,
+                "title": title
             })
         else:
             return render(request, "encyclopedia/error.html", {
                 "error": "404 Error, this page does not exist"
             })
+            
